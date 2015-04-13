5b7271f1477b51009721c587ba67d46ce6af9dc0
amit <amit@amit-Lenovo-G560.(none)>
2015-04-13 11:27:49 +0530

    Product Searchby Internal Reference

---
+++ __openerp__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Product Extended',
+    'version': '1.0',
+    'category': 'Product',
+    'author': 'Genpex',
+    'description' : 'This module help to search the product by internal reference(Product Default Code).',
+    'website': 'http://www.genpex.com',
+    'depends': ['base', 'product'],
+    'data': ['product_ext_view.xml'],
+    'demo': [],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
